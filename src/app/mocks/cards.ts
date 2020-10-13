700cabfc8457dc6b925126ae69b99e8a6d815a31
Irenia <maryirenia@gmail.com>
2020-10-12 21:47:36 -0600

    [add] Mockup cards

---
+++ src/app/mocks/cards.ts
@@ -0,0 +1,56 @@
+export const cards =
+[
+  {
+    "id": "1",
+    "name": "Google Play",
+    "image": "https://media.karousell.com/media/photos/products/2020/5/21/rm50_goggle_play_gift_card_mal_1590040469_c1100b5a_progressive.jpg",
+    "chargeRate": 1,
+    "availability": [
+      1,
+      2,
+      3,
+      4
+    ]
+  },
+  {
+    "id": "2",
+    "name": "PlayStation",
+    "image": "https://www.allkeyshop.com/blog/wp-content/uploads/PlayStationNetworkGiftCard.jpg",
+    "chargeRate": 0.25,
+    "availability": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "id": "3",
+    "name": "Steam",
+    "image": "https://mojolika.com/wp-content/uploads/2019/04/196.png",
+    "chargeRate": 0.1,
+    "availability": [
+      1,
+      3
+    ]
+  },
+  {
+    "id": "4",
+    "name": "Amazon",
+    "image": "https://www.shopmyexchange.com/products/images/xlarge/2008097_0000.jpg",
+    "chargeRate": 0.5,
+    "availability": [
+      2,
+      4
+    ]
+  },
+  {
+    "id": "5",
+    "name": "Microsoft",
+    "image": "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1XIn1?ver=1a7a",
+    "chargeRate": 0.75,
+    "availability": [
+      2,
+      3
+    ]
+  }
+]
