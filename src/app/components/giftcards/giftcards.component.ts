eccee1bc4c9ad9765df5554c4d231f9b09ec974b
Estuardo Gómez <esthuardo12@gmail.com>
2020-10-28 17:08:59 -0600

    Se agregó la opción de regalar las giftcards

--- src/app/components/giftcards/giftcards.component.ts
+++ src/app/components/giftcards/giftcards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {giftcards} from '../../mocks/giftcards';
+import {users} from '../../mocks/users';
 
 @Component({
   selector: 'app-giftcards',
@@ -8,12 +9,27 @@ import {giftcards} from '../../mocks/giftcards';
 })
 export class GiftcardsComponent implements OnInit {
   public datos: any;
+  public usuarios: any;
+  public tj = -1;
+  public user =  '';
   constructor(
   ) { }
-
+  public dialogo = document.getElementById('dialogo');
   ngOnInit(): void {
     this.datos = giftcards;
-   // console.log(giftcards);
+    this.usuarios = users;
+    console.log(giftcards);
   }
 
+    boton(tarjeta): void {
+      console.log('A regalar tarjeta con el id: ' + tarjeta);
+      this.tj = tarjeta;
+    }
+
+    regalar(): void {
+      console.log('A regalar la tarjeta: ' + this.tj + ' al usuario: ' + this.user);
+    }
+    registrarusuario(u): void {
+      this.user = u;
+    }
 }
